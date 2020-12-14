4114c92b134e7cdd572a7a4d7cad9ba2b5c9871c
adriancampbell <50539588+acampbell2499@users.noreply.github.com>
2020-12-14 18:54:17 -0500

    Se agregó Simulation Template.

---
+++ SimulationTemplate/SimulationTemplate/include/CollisionDetection.h
@@ -0,0 +1,53 @@
+#include <Box2D/include/box2d.h>
+#include <iostream>
+
+using namespace std;
+
+#include <Box2D/include/box2d.h>
+#include <iostream>
+
+using namespace std;
+
+class MyContactListener : public b2ContactListener
+{
+    bool longBoolean;
+    void BeginContact(b2Contact* contact)
+    {
+
+        b2Fixture* f1 = contact->GetFixtureA();
+        b2Fixture* f2 = contact->GetFixtureB();
+
+        b2FixtureUserData o1 = f1->GetUserData();
+        b2FixtureUserData o2 = f2->GetUserData();
+
+        longBoolean = (o1.pointer == 19 && o2.pointer == 27) || (o1.pointer == 19 && o2.pointer == 1) || (o1.pointer == 19 && o2.pointer == 2) || (o1.pointer == 19 && o2.pointer == 3) || (o1.pointer == 19 && o2.pointer == 4) || (o1.pointer == 19 && o2.pointer == 5);
+
+        if (longBoolean)
+        {
+            onAir = false;
+            cout << "Ground touched" << endl;
+        }
+    }
+
+    void EndContact(b2Contact* contact)
+    {
+        b2Fixture* f1 = contact->GetFixtureA();
+        b2Fixture* f2 = contact->GetFixtureB();
+
+        b2FixtureUserData o1 = f1->GetUserData();
+        b2FixtureUserData o2 = f2->GetUserData();
+
+        longBoolean = (o1.pointer == 19 && o2.pointer == 27) || (o1.pointer == 19 && o2.pointer == 1) || (o1.pointer == 19 && o2.pointer == 2) || (o1.pointer == 19 && o2.pointer == 3) || (o1.pointer == 19 && o2.pointer == 4) || (o1.pointer == 19 && o2.pointer == 5);
+
+
+        if (longBoolean)
+        {
+            onAir = true;
+            cout << "Ground left" << endl;
+        }
+
+    }
+
+public:
+    bool onAir = false;
+};
